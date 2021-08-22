b5228627e12e370b5ea1a2b71f02eeb88a740b14
Jason <tehblackboy@gmail.com>
2021-08-22 00:20:32 -0400

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.ApplicationModel.Core;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Video_Player
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private bool locked = false;
+        private readonly List<string> fileTypes = new List<string>() { ".mp4", ".mov", ".flv", ".wmv", ".mkv" };
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+
+            Window.Current.SetTitleBar(CustomDragRegion);
+            Window.Current.CoreWindow.CharacterReceived += CoreWindow_CharacterReceived;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter != null && e.Parameter is List<StorageFile> list)
+            {
+                var files = list;
+                foreach (var f in files)
+                {
+                    AddTab(f);
+                }
+            }
+        }
+
+        private async void showMessage(string message)
+        {
+            var messageDialog = new MessageDialog(message);
+
+            // Set the command that will be invoked by default
+            messageDialog.DefaultCommandIndex = 0;
+
+            // Set the command to be invoked when escape is pressed
+            messageDialog.CancelCommandIndex = 1;
+
+            // Show the message dialog
+            await messageDialog.ShowAsync();
+        }
+
+        private void MainPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ResizeTabs();
+        }
+
+        private void CoreWindow_CharacterReceived(CoreWindow sender, CharacterReceivedEventArgs args)
+        {
+            TabViewItem tab = (TabViewItem)Tabs.SelectedItem;
+            MyVideoControl videoControl = tab.Content as MyVideoControl;
+            switch (args.KeyCode)
+            {
+                case 32:
+                    videoControl.PlayPause();
+                    break;
+                case 93:
+                    videoControl.MediaPlayer.MediaPlayer.PlaybackSession.PlaybackRate += 0.1;
+                    break;
+                case 91:
+                    videoControl.MediaPlayer.MediaPlayer.PlaybackSession.PlaybackRate -= 0.1;
+                    break;
+                case 106:
+                    videoControl.MediaPlayer.MediaPlayer.PlaybackSession.Position -= TimeSpan.FromSeconds(5);
+                    break;
+                case 108:
+                    videoControl.MediaPlayer.MediaPlayer.PlaybackSession.Position += TimeSpan.FromSeconds(5);
+                    break;
+                default:
+                    System.Diagnostics.Debug.WriteLine(args.KeyCode);
+                    break;
+            }
+        }
+
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (FlowDirection == FlowDirection.LeftToRight)
+            {
+                CustomDragRegion.MinWidth = sender.SystemOverlayRightInset;
+                ShellTitlebarInset.MinWidth = sender.SystemOverlayLeftInset;
+            }
+            else
+            {
+                CustomDragRegion.MinWidth = sender.SystemOverlayLeftInset;
+                ShellTitlebarInset.MinWidth = sender.SystemOverlayRightInset;
+            }
+
+            CustomDragRegion.Height = ShellTitlebarInset.Height = sender.Height;
+        }
+
+        public void AddTab(StorageFile file)
+        {
+            OpenButton.Visibility = Visibility.Collapsed;
+            MyVideoControl videoControl = new MyVideoControl(file);
+            TabViewItem tab = new TabViewItem() { MinWidth = 40, Header = file.Name, AllowDrop = true, Content = videoControl };
+            Tabs.TabItems.Add(tab);
+            ResizeTabs();
+            if (Tabs.TabItems.Count == 1)
+            {
+                Tabs.SelectedIndex = 0;
+            }
+        }
+
+        private void ResizeTabs()
+        {
+            Tabs.CloseButtonOverlayMode = TabViewCloseButtonOverlayMode.OnPointerOver;
+            foreach (TabViewItem tabViewItem in Tabs.TabItems)
+            {
+                tabViewItem.MaxWidth = Math.Min(400, (Window.Current.Bounds.Width - 200) / Tabs.TabItems.Count);
+            }
+        }
+
+        private void Tab_CloseRequested(TabView sender, TabViewTabCloseRequestedEventArgs args)
+        {
+            MyVideoControl videoControl = args.Tab.Content as MyVideoControl;
+            videoControl.MediaPlayer.MediaPlayer.Pause();
+            //videoControl.MediaPlayer.MediaPlayer.Dispose();
+            sender.IsAddTabButtonVisible = true;
+            sender.TabItems.Remove(args.Tab);
+            sender.IsAddTabButtonVisible = false;
+            ResizeTabs();
+        }
+
+        private async void Page_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                if (locked) return;
+                locked = true;
+                var items = await e.DataView.GetStorageItemsAsync();
+                if (items.Count > 0)
+                {
+                    foreach (StorageFile appFile in items.OfType<StorageFile>())
+                    {
+                        if (fileTypes.Contains(appFile.FileType)) 
+                        {
+                            AddTab(appFile);
+                        }
+                    }
+                }
+                locked = false;
+            }
+        }
+
+        private void Drag_over(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+        }
+
+        private async void OpenFilePicker()
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker
+            {
+                ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail,
+                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop
+            };
+
+            foreach (var fileType in fileTypes) 
+            {
+                picker.FileTypeFilter.Add(fileType);
+            }
+
+            var files = await picker.PickMultipleFilesAsync();
+            if (files.Count > 0)
+            {
+                // Application now has read/write access to the picked file(s)
+                foreach (StorageFile file in files)
+                {
+                    if (file != null)
+                    {
+                        AddTab(file);
+                    }
+                }
+            }
+
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFilePicker();
+        }
+    }
+}
---
+++ MyVideoControl.xaml.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Storage;
+using Windows.System.Threading;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Video_Player
+{
+    public sealed partial class MyVideoControl : UserControl
+    {
+        private MediaPlaybackState playbackState;
+        private TimeSpan a = TimeSpan.FromMilliseconds(0);
+        private TimeSpan b = TimeSpan.FromMilliseconds(0);
+        private ThreadPoolTimer PeriodicTimer;
+        private bool pingpong = false;
+        public MyVideoControl(StorageFile file)
+        {
+            InitializeComponent();
+
+            mediaPlayer.Source = MediaSource.CreateFromStorageFile(file);
+
+            mediaPlayer.MediaPlayer.PlaybackSession.PositionChanged += PlaybackSession_PositionChanged;
+            mediaPlayer.MediaPlayer.PlaybackSession.PlaybackStateChanged += PlaybackSession_PlaybackStateChanged;
+            mediaPlayer.MediaPlayer.MediaOpened += MediaPlayer_MediaOpened;
+            mediaPlayer.MediaPlayer.PlaybackSession.PlaybackRateChanged += PlaybackSession_PlaybackRateChanged;
+        }
+
+        private async void PlaybackSession_PlaybackRateChanged(MediaPlaybackSession sender, object args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                UpdatePlaybackSpeed();
+            });
+        }
+
+        private async void MediaPlayer_MediaOpened(MediaPlayer sender, object args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                UpdateTimestamp();
+                UpdatePlaybackSpeed();
+                b = mediaPlayer.MediaPlayer.PlaybackSession.NaturalDuration;
+            });
+        }
+
+        private async void PlaybackSession_PlaybackStateChanged(MediaPlaybackSession sender, object args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                switch (sender.PlaybackState)
+                {
+                    case MediaPlaybackState.Playing:
+                        playPause.Content = new SymbolIcon(Symbol.Pause);
+                        break;
+                    case MediaPlaybackState.Paused:
+                        playPause.Content = new SymbolIcon(Symbol.Play);
+                        break;
+                    default:
+                        break;
+                }
+            });
+        }
+
+        /**
+         * Update slider and timestamp from player position.
+         */
+        private void updateFromPlayer()
+        {
+            slider.ValueChanged -= slider_ValueChanged;
+            slider.Value = mediaPlayer.MediaPlayer.PlaybackSession.Position.TotalSeconds / mediaPlayer.MediaPlayer.PlaybackSession.NaturalDuration.TotalSeconds * 100;
+            UpdateTimestamp();
+            slider.ValueChanged += slider_ValueChanged;
+        }
+
+        /**
+         * Updates the video position and timestamp from the slider value.
+         */
+        private void UpdateFromSlider()
+        {
+            //System.Diagnostics.Debug.WriteLine("update from slider");
+            mediaPlayer.MediaPlayer.PlaybackSession.Position = TimeSpan.FromMilliseconds(slider.Value / 100 * mediaPlayer.MediaPlayer.PlaybackSession.NaturalDuration.TotalMilliseconds);
+            UpdateTimestamp();
+        }
+
+        /**
+         * Updates the timestamp based on the media player position.
+         */
+        private void UpdateTimestamp()
+        {
+            int milliseconds = (int)mediaPlayer.MediaPlayer.PlaybackSession.Position.TotalMilliseconds;
+            int seconds = milliseconds / 1000;
+            int minutes = seconds / 60;
+            int totalMilliseconds = (int)mediaPlayer.MediaPlayer.PlaybackSession.NaturalDuration.TotalMilliseconds;
+            int totalSeconds = totalMilliseconds / 1000;
+            int totalMinutes = totalSeconds / 60;
+            seconds %= 60;
+            totalSeconds %= 60;
+            timestamp.Text = String.Format("{0}:{1:D2}/{2}:{3:D2}", minutes, seconds, totalMinutes, totalSeconds);
+        }
+
+        private void UpdatePlaybackSpeed()
+        {
+            videoSpeedLabel.Text = String.Format("{0:F2}", mediaPlayer.MediaPlayer.PlaybackSession.PlaybackRate);
+        }
+
+        private bool IsPositionBetweenAandB(TimeSpan position, TimeSpan fullVideoLength)
+        {
+            TimeSpan stop = b;
+            TimeSpan pos = position;
+            if (a > b)
+            {
+                stop += fullVideoLength;
+                if (pos < b)
+                {
+                    pos += fullVideoLength;
+                }
+            }
+            return a < pos && pos < stop;
+        }
+
+        private async void PlaybackSession_PositionChanged(MediaPlaybackSession sender, object args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                 {
+                     updateFromPlayer();
+                     /*if ((bool)toggleB.IsChecked && Math.Abs(mediaPlayer.MediaPlayer.PlaybackSession.Position.TotalMilliseconds - b.TotalMilliseconds) < 50)
+                     {
+                         mediaPlayer.MediaPlayer.PlaybackSession.Position = a;
+                     }*/
+                     TimeSpan position = mediaPlayer.MediaPlayer.PlaybackSession.Position;
+                     TimeSpan fullVideoLength = mediaPlayer.MediaPlayer.PlaybackSession.NaturalDuration;
+                     if (!IsPositionBetweenAandB(position, fullVideoLength))
+                     {
+                         mediaPlayer.MediaPlayer.PlaybackSession.Position = a;
+                     }
+                 });
+
+        }
+
+        public MediaPlayerElement MediaPlayer { get { return mediaPlayer; } }
+
+        public void PlayPause(object sender = null, Windows.UI.Xaml.RoutedEventArgs e = null)
+        {
+            ReverseVideo(false);
+            if (mediaPlayer.MediaPlayer.PlaybackSession.PlaybackState == MediaPlaybackState.Playing)
+            {
+                mediaPlayer.MediaPlayer.Pause();
+            }
+            else
+            {
+                mediaPlayer.MediaPlayer.Play();
+            }
+        }
+
+        private void slider_ManipulationStarted(object sender, Windows.UI.Xaml.Input.ManipulationStartedRoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Slider manipulation started");
+            PrepareForSeek();
+        }
+
+        private void Slider_ManipulationCompleted(object sender, Windows.UI.Xaml.Input.ManipulationCompletedRoutedEventArgs e)
+        {
+            UpdateFromSlider();
+            FinishedSeeking();
+        }
+
+        /**
+         * Prepare to use the seekbar to change the position of the video by removing listeners and saving the current playback state.
+         */
+        private void PrepareForSeek()
+        {
+            playbackState = mediaPlayer.MediaPlayer.PlaybackSession.PlaybackState;
+            mediaPlayer.MediaPlayer.Pause();
+            mediaPlayer.MediaPlayer.PlaybackSession.PositionChanged -= PlaybackSession_PositionChanged;
+            Debug.WriteLine(mediaPlayer.MediaPlayer.PlaybackSession.PlaybackState);
+        }
+
+        /**
+         * Re-add the media progress listener after seeking has been completed.
+         */
+        private void FinishedSeeking()
+        {
+            switch (playbackState)
+            {
+                case MediaPlaybackState.Playing:
+                    mediaPlayer.MediaPlayer.Play();
+                    break;
+                case MediaPlaybackState.Paused:
+                    break;
+                default:
+                    break;
+            }
+            mediaPlayer.MediaPlayer.PlaybackSession.PositionChanged += PlaybackSession_PositionChanged;
+        }
+
+        private void slider_ValueChanged(object sender, Windows.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e)
+        {
+            UpdateFromSlider();
+        }
+
+        private void toggle_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (sender == toggleA)
+            {
+                a = (bool)toggleA.IsChecked ? mediaPlayer.MediaPlayer.PlaybackSession.Position : TimeSpan.FromMilliseconds(0);
+            }
+            else if (sender == toggleB)
+            {
+                b = (bool)toggleB.IsChecked ? mediaPlayer.MediaPlayer.PlaybackSession.Position : mediaPlayer.MediaPlayer.PlaybackSession.NaturalDuration;
+            }
+            else if (sender == toggleLoop)
+            {
+                mediaPlayer.MediaPlayer.IsLoopingEnabled = (bool)toggleLoop.IsChecked;
+            }
+        }
+
+        private void MenuFlyoutItem_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (sender == flipH)
+            {
+                scale.ScaleX *= -1;
+            }
+            else if (sender == rotateR || sender == rotateL)
+            {
+                rotate.Angle += sender == rotateR ? 90 : -90;
+                UpdateLayout();
+            }
+        }
+
+
+        private void ReverseVideo(bool start)
+        {
+            if (start)
+            {
+                var period = TimeSpan.FromMilliseconds(30);
+                PeriodicTimer = ThreadPoolTimer.CreatePeriodicTimer((source) =>
+                {
+                    _ = Dispatcher.RunAsync(CoreDispatcherPriority.High, () =>
+                    {
+                        if (mediaPlayer.MediaPlayer.PlaybackSession.Position > TimeSpan.Zero)
+                        {
+                            mediaPlayer.MediaPlayer.PlaybackSession.Position -= period;
+                        }
+                        else
+                        {
+                            source.Cancel();
+                            reverseButton.IsChecked = false;
+                        }
+                    });
+
+                }, period);
+            }
+            else
+            {
+                if (PeriodicTimer != null)
+                {
+                    PeriodicTimer.Cancel();
+                }
+                reverseButton.IsChecked = false;
+            }
+        }
+
+        private void reverseButton_Checked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (mediaPlayer.MediaPlayer.PlaybackSession.PlaybackState == MediaPlaybackState.Playing)
+            {
+                PlayPause();
+            }
+            ReverseVideo((bool)reverseButton.IsChecked);
+        }
+    }
+}
